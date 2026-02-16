947936f76c2fb5a460659f259f5197699f64fe29
Greiman Sánchez <sanchezgreiman@gmail.com>
2026-02-15 21:34:55 -0300

    feat: improve user search functionality and profile menu interaction

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,45 +19,22 @@
                 </a>
 
                 @auth
-                <div class="relative">
-                    <input type="text" id="busqueda" placeholder="Buscar usuarios..." class="border-2 p-2 w-full">
-                        
-                    <ul id="resultados" class="absolute bg-white w-full border mt-1 hidden shadow-lg z-50">
+                <div class="relative rounded-full py-2 bg-gray-100 text-gray-700 max-w-md flex-1">
+    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                        class="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-500">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+
+                    <input type="text" id="busqueda" placeholder="Buscar en Devstagram..." 
+                        autocomplete="off" class="focus:ring-0 focus:outline-none bg-transparent text-gray-800 border-none w-full pl-11 pr-5">
+                    
+                    <ul id="resultados" class="absolute top-full left-0 bg-white w-full rounded-xl shadow-lg z-50 hidden border border-gray-100">
+                        <li class="px-4 py-2 hover:bg-gray-50 cursor-pointer first:rounded-t-xl last:rounded-b-xl">Resultado 1</li>
                     </ul>
-                    </div>
-                        <script>
-                            const input = document.querySelector('#busqueda');
-                            const resultadosUl = document.querySelector('#resultados');
+                </div>
 
-                            input.addEventListener('input', async (e) => {
-                                const query = e.target.value;
-
-                                if (query.length < 2) {
-                                    resultadosUl.classList.add('hidden');
-                                    return;
-                                }
-
-                                // Llamamos a nuestra ruta de Laravel
-                                const response = await fetch(`/buscar?query=${query}`);
-                                const usuarios = await response.json();
-
-                                // Limpiamos y mostramos resultados
-                                resultadosUl.innerHTML = '';
-                                if (usuarios.length > 0) {
-                                    usuarios.forEach(user => {
-                                        const li = document.createElement('li');
-                                        li.className = 'p-2 hover:bg-gray-100 cursor-pointer';
-                                        li.innerHTML = `<a href="/${user.username}" class="block w-full h-full p-2">${user.username}</a>`;
-                                        resultadosUl.appendChild(li);
-                                    });
-                                    resultadosUl.classList.remove('hidden');
-                                } else {
-                                    resultadosUl.classList.add('hidden');
-                                }
-                            });
-                        </script>
-
-                    <nav class="flex gap-2 items-center">
+                    <nav class="flex items-center px-3 gap-4">
                         <a
                             class="flex items-center gap-2 bg-white border p-2 text-gray-600
                             rounded text-sm uppercase font-bold cursor-pointer"
@@ -72,21 +49,37 @@
                             Crear
 
                         </a>
-                        <a class="font-bold text-gray-600 text-sm" href="{{ route('posts.index', auth()->user()->username) }}"
->
-                            Hola: 
-                            <span class="font-normal"> 
-                                {{ auth()->user()-> username }}
+                        
+                        <a class="font-bold text-gray-600 text-sm gap-2 flex items-center whitespace-nowrap">
+                            <span class="font-bold text-gray-600 text-sm"> 
+                                {{ auth()->user()->username }}
                             </span>
                         </a>
 
-                        <form method="POST" action=" {{route('logout')}} ">
-                            @csrf
-                            <button type="submit" class="font-bold uppercase
-                             text-gray-600 text-sm">
-                                Cerrar Sesion
-                            </button>
-                        </form>
+                        <div class="relative md:w-8/12 lg:w-6/12" href="{{ route('perfil.index') }}">
+                            <img 
+                                id="profileBtn"
+                                src="{{ 
+                                auth()->user()->imagen ? 
+                                asset('storage/' . auth()->user()->imagen) : 
+                                asset('img/usuario.svg') }}"
+                                alt="imagen usuario"
+                                class="w-10 h-10 rounded-full cursor-pointer"/>
+
+                            <!-- Dropdown -->
+                        <div 
+                            id="profileMenu"
+                            class="hidden absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md cursor-pointer"
+                        >
+                            <a class="block px-4 py-2 hover:bg-gray-100" href="{{ route('posts.index', auth()->user()->username) }}">Perfil</a>
+                            <form class="block px-4 py-2 hover:bg-gray-100" 
+                            method="POST" action="{{route('logout')}}">
+                                @csrf
+                                <button type="submit" class="cursor-pointer">
+                                    Cerrar Sesion
+                                </button>
+                            </form>
+                        </div>
                     </nav>
                 @endauth
 
@@ -114,6 +107,65 @@
         </footer>
 
         @livewireScripts
+
+        <script>
+            const input = document.querySelector('#busqueda');
+            const resultadosUl = document.querySelector('#resultados');
+
+            input.addEventListener('input', async (e) => {
+                const query = e.target.value.trim(); // .trim() para evitar búsquedas de puros espacios
+
+                if (query.length < 2) {
+                    resultadosUl.innerHTML = '';
+                    resultadosUl.classList.add('hidden');
+                    return;
+                }
+
+                try {
+                    // Llamar ruta de Laravel
+                    const response = await fetch(`/buscar?query=${query}`);
+                    const usuarios = await response.json();
+
+                    // Limpiar resultados anteriores
+                    resultadosUl.innerHTML = '';
+                    // Mostrar la lista si hay al menos 2 caracteres
+                    resultadosUl.classList.remove('hidden');
+
+                    if (usuarios.length > 0) {
+                        // Si hay usuarios, listado
+                        usuarios.forEach(user => {
+                            const li = document.createElement('li');
+                            li.className = 'hover:bg-gray-100 cursor-pointer transition-colors';
+                            li.innerHTML = `<a href="/${user.username}" class="block w-full px-4 py-2 text-sm text-gray-700">${user.username}</a>`;
+                            resultadosUl.appendChild(li);
+                        });
+                    } else {
+                        // Si no hay resltados, mensaje de no encontrado
+                        const liNoResults = document.createElement('li');
+                        liNoResults.className = 'px-4 py-3 text-sm text-gray-500 italic';
+                        liNoResults.textContent = 'No se encontraron resultados para "' + query + '"';
+                        resultadosUl.appendChild(liNoResults);
+                    }
+                } catch (error) {
+                    console.error("Error al buscar:", error);
+                }
+            });
+        </script>
+        <script>
+            const profileBtn = document.getElementById('profileBtn');
+            const profileMenu = document.getElementById('profileMenu');
+
+            profileBtn.addEventListener('click', () => {
+                profileMenu.classList.toggle('hidden');
+            });
+
+            // Cerrar si haces click fuera
+            document.addEventListener('click', (e) => {
+                if (!profileBtn.contains(e.target) && !profileMenu.contains(e.target)) {
+                    profileMenu.classList.add('hidden');
+                }
+            });
+        </script>
     </body>
 
 </html>
